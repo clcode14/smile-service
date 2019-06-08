939602f7bdb06c5d3e855499e99d1e92be17f964
蔡磊47872 <cl47872@ly.com>
2019-06-08 15:26:04 +0800

    修改对象参数接口请求方式

--- smile_admin/src/main/java/org/flightythought/smile/admin/controller/UserController.java
+++ smile_admin/src/main/java/org/flightythought/smile/admin/controller/UserController.java
@@ -8,7 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,7 +26,7 @@ public class UserController {
     @Autowired
     private AppUserService appUserService;
     
-    @GetMapping("/list")
+    @PostMapping("/list")
     @ApiOperation(value = "用户信息列表", notes = "获取用户信息列表")
     public ResponseBean findUsers(@RequestBody AppUserQueryDTO appUserQueryDTO) {
         try {
