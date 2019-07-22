74e7e1e5fb5ad57ecf0e3eadf8e86f74d2a396c0
valence <valence@aliyun.com>
2019-07-23 01:47:01 +0800

    添加参数，移除eureka连接

--- smile_admin/src/main/java/org/flightythought/smile/admin/AdminApplication.java
+++ smile_admin/src/main/java/org/flightythought/smile/admin/AdminApplication.java
@@ -2,10 +2,9 @@ package org.flightythought.smile.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 public class AdminApplication {
 
     public static void main(String[] args) {
